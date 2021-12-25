09374e6b03aa123c5e44b9bcb2c1c118df85b0fb
Administrator <pbull809@gmail.com>
2021-12-25 20:05:50 +0100

    save

--- src/SensoGame.java
+++ src/SensoGame.java
@@ -6,19 +6,19 @@ import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.BufferedInputStream;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Objects;
+import java.util.*;
 import java.util.Timer;
-import java.util.TimerTask;
 
 public class SensoGame extends JPanel {
     public BufferedImage imgMain;
     public BufferedImage imgLight;
     private BufferedImage offImage;
     private Graphics offGraphics;
-    private boolean clickAllow = true;
+    private boolean clickAllowed = true;
+    private Random rnd = new Random();
     public ArrayList<Integer> mission = new ArrayList<>();
     public ArrayList<Integer> input = new ArrayList<>();
+    private boolean userPlays = false;
 
     public SensoGame() throws Exception {
         super();
@@ -26,19 +26,58 @@ public class SensoGame extends JPanel {
         addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
-                if (clickAllow == false)
+                e.consume();
+                if (clickAllowed == false)
                     return;
-
+                if (userPlays == false)
+                    return;
                 int x = e.getX()*2/getWidth();
                 int y = e.getY()*2/getHeight();
                 int pos = x+y*2;
-                switchLight (pos);
+                switchLight (pos, 300);
                 input.add(pos);
+                //System.out.println(input.size()+ " -- "+ mission.size());
+                if (input.size() == mission.size()) {
+                    if (!input.toString().contentEquals(mission.toString()))
+                    {
+                        System.out.println("fail");
+                    }
+                    input.clear();
+                    userPlays = false;
+                }
             }
         });
+
+        new Thread(() -> {
+        for (;;) {
+            if (userPlays == true)
+            {
+                wait (2000);
+                continue;
+            }
+            // computer plays
+            mission.add(Math.abs(rnd.nextInt()%4));
+            System.out.println(mission);
+            for (Integer i : mission)
+            {
+                switchLight (i, 1000);
+                wait (1000);
+            }
+            userPlays = true;
+        }
+        }).start();
     }
 
-    private void switchLight(int pos) { // 0...3
+    private void wait (int x)
+    {
+        try {
+            Thread.sleep(x);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private synchronized void switchLight(int pos, int delay) { // 0...3
         switch (pos) {
             case 0:
                 offGraphics.setClip(0, 0,
@@ -63,23 +102,28 @@ public class SensoGame extends JPanel {
         }
         offGraphics.drawImage (imgLight, 0,0, null);
         repaint();
-        clickAllow = false;
+        clickAllowed = false;
 
         new Timer().schedule(new TimerTask() {
             @Override public void run() {
                 offGraphics.setClip(null);
                 offGraphics.drawImage (imgMain, 0,0, null);
                 repaint();
-                clickAllow = true;
+                clickAllowed = true;
             }
-        }, 300);
+        }, delay);
     }
 
     @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
+    public void paint(Graphics g) {
+        //super.paintComponent(g);
         g.drawImage (offImage, 0,0, getWidth(), getHeight(), this);
     }
+
+    @Override
+    public void update(Graphics g) {
+    }
+
 
     private void prepareGraphic (String face, String light) throws Exception {
         imgMain = ImageIO.read (Objects.requireNonNull (getResource (face)));
